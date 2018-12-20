b6f6a8df663ee009f72388e1655438a59ea35826
Mike Levan <mlevan1992@gmail.com>
2018-12-20 15:18:06 -0500

    Build a new manifest dynamically vs having to add static values all of the time

---
+++ build_dynamic_k8s_manifest.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+
+
+def k8s_manifest(*kwargs):
+    template = """apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: {name}
+    spec:
+      selector:
+        matchLabels:
+          app: nginx
+      replicas: 2
+      template:
+        metadata:
+          labels:
+            app: nginx
+        spec:
+          containers:
+          - name: {name}
+            image: {image}
+            ports:
+            - containerPort: 80
+    """
+
+    K8s_manifest = template.format(name=name, image=image)
+    print(K8s_manifest)
+
+    yaml_file = os.path.isfile("{}/manifest.yaml").format(file_path)
+
+    if yaml_file:
+        os.system("rm -rf {}/manifest.yaml").format(file_path)
+        os.system("touch {}/manifest.yaml").format(file_path)
+
+        with open("{}/manifest.yaml", "w").format(file_path) as f:
+            f.write(K8s_manifest)
+    else:
+        os.system("touch {}/manifest.yaml").format(file_path)
+
+        with open("{}/manifest.yaml", "w").format(file_path) as f:
+            f.write(K8s_manifest)
+
+
+name = sys.argv[1]
+image = sys.argv[2]
+file_path = sys.argv[3]
+
+k8s_manifest(name, image, file_path)
